c490096b7013f076d0e9903b7ddb892b46cdfcaa
Filip Borovec <filip.borovec@creativedock.cz>
2018-11-11 13:51:04 +0100

    fix running integration tests

--- web/src/test/java/cz/openwise/userpost/integration/UserPostIT.java
+++ web/src/test/java/cz/openwise/userpost/integration/UserPostIT.java
@@ -7,11 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 /**
- * Test suit for User post service.
+ * Integration test suit for User post service.
  *
  * @author <a href="mailto:filip.borovec@openwise.cz">Filip Borovec</a>
  */
-public class UserPostIntegrationTest extends AbstractIntegrationTest {
+public class UserPostIT extends AbstractIntegrationTest {
 
     private static final long USER_ID = 1L;
     private static final long NON_EXISTING_USER_ID = 20L;
@@ -37,8 +37,7 @@ public class UserPostIntegrationTest extends AbstractIntegrationTest {
                 .get().uri("/users/{id}", NON_EXISTING_USER_ID)
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isNotFound();
+                .expectStatus()
+                .isNotFound();
     }
-
-
 }
